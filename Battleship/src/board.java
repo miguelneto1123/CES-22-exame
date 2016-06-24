91f5ed3bd6ed59be88101ee1d33d423b649d19b9
miguel <miguel@miguel-VPCEA36FM.(none)>
2016-06-24 05:51:50 -0300

    Escrevendo funcoes uteis para o board

--- Battleship/src/board.java
+++ Battleship/src/board.java
@@ -29,5 +29,55 @@ public class board {
         }
 
     }
+    
+    public String boardString (int[][] board) {
+    	String res = "";
+    	
+    	res = res + "\t1 \t2 \t3 \t4 \t5 \t6 \t7\n\n";
+        
+        for(int row = 0; row < 7; row++){
+        	res = res + (row+1) + "\n";
+            for (int column = 0; column < 7; column++) {
+            	
+                if (board[row][column] == -1) {
+                	res = res + "\t~\n";
+                } else if (board[row][column] == 0) {
+                	res = res + "\t*\n";
+                } else if (board[row][column] == 1) {
+                	res = res + "\tX\n";
+                }
+                
+            }
+        }
+        
+        return res;
+    }
+    
+    public String unveiledBoardString(int[][] board, int [][] ships){
+    	String res = "";
+    	res = res + "\t1 \t2 \t3 \t4 \t5 \t6 \t7\n\n";
+    	
+    	for (int row = 0; row < 7; row++){
+            res = res + (row+1) + "\n";
+            
+            for (int column = 0; column < 7; column++) {
+            	boolean hasShip = false;
+            	for (int s = 0; s < ships.length && !hasShip; s++) {
+            		if (ships[s][0] == row && ships[s][1] == column) {
+            			res = res + "\tX\n";
+            			hasShip = true;
+            		}
+            	}
+            	if (!hasShip){
+	                if (board[row][column] == -1) {
+	                    res = res + "\t~\n";
+	                } else if (board[row][column] == 0) {
+	                    res = res + "\t*\n";
+	                }
+            	}
+            }
+        }
+    	return res;
+    }
 	
 }
